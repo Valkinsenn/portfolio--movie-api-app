a56ffcc650b6bdadaa30db03276f2dbe05320081
Valkinsenn <valkinsenn@gmail.com>
2021-10-19 00:37:24 -0500

    Filling the singular movie panel, further updates to style (almost there!!!)

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ const searchApi = `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5
 const closeButton = document.querySelector(".close-button")
 const movieCheckbox = document.querySelector("#display-checkbox")
 const movieBoxDisplay = document.querySelector("#display-box")
-const moviePanel = document.querySelector(".movie-panel")
+const moviePanel = document.querySelector(".panel")
 const pageContent = document.querySelector(".page-content")
 const resetButton = document.querySelector(`#site-logo`)
 const searchForm = document.querySelector("#search-form")
@@ -68,12 +68,7 @@ const showMovies = async (apiUrl, genreUrl) => {
     .then(data =>
       data.results.forEach((element, i) => {
         // For testing purposes:
-        console.log(element)
-
-        // Filters the list of genres by ID, then gets the name of each genre:
-        const genres = genresList
-          .filter(object => element.genre_ids.includes(object.id))
-          .map(item => item.name)
+        // console.log(element)
 
         setTimeout(() => {
           // Sets up the main movie element display:
@@ -83,41 +78,35 @@ const showMovies = async (apiUrl, genreUrl) => {
             movieBoxDisplay.appendChild(movieElement)
           }
 
-          // Sets up the movie's poster:
+          // POSTER DISPLAY:
           const moviePoster = document.createElement("img")
           moviePoster.classList.add("movie-poster")
           movieElement.appendChild(moviePoster)
           moviePoster.src = `${imgPath}${element.poster_path}`
 
-          // In case the poster doesn't load:
-          moviePoster.addEventListener("error", () => {
-            console.log(`Well, shit! I didn't load! :'(`)
-          })
-
-          // Sets up the movie details display:
+          // DETAILS DISPLAY:
           const movieDetails = document.createElement("div")
           movieDetails.classList.add(`movie-details`)
           movieElement.appendChild(movieDetails)
 
-          // Sets up the movie's title:
+          // TITLE DISPLAY:
           const movieTitle = document.createElement("h2")
           movieTitle.classList.add("movie-title")
           movieDetails.appendChild(movieTitle)
           movieTitle.innerHTML = `${element.title}`
           // console.log(element.title)
 
-          // Sets up the movie description:
+          // DESCRIPTION DISPLAY:
           const movieDesc = document.createElement("div")
           movieDesc.classList.add("movie-description")
           movieDetails.appendChild(movieDesc)
 
-          // Sets up the movie's year in the description:
+          // YEAR DISPLAY:
           const movieYear = document.createElement("h3")
           movieYear.classList.add("movie-year")
           movieDesc.appendChild(movieYear)
 
-          // Sometimes, movies don't have a year.
-          // This is for correcting that little error:
+          // IF THERE'S NO YEAR:
           if (element.release_date) {
             movieYear.innerHTML = `${dateFns.format(
               element.release_date,
@@ -127,24 +116,24 @@ const showMovies = async (apiUrl, genreUrl) => {
             movieYear.innerHTML = `N/A`
           }
 
-          // Sets up the movie's rating box in the description:
+          // RATING BOX DISPLAY:
           const movieRatingBox = document.createElement("div")
           movieRatingBox.classList.add("movie-rating-box")
           movieDesc.appendChild(movieRatingBox)
 
-          // Sets up the movie's actual rating (out of ten):
+          // RATING (OUT OF TEN):
           const movieRating = document.createElement("h4")
           movieRating.classList.add("movie-rating")
           movieRatingBox.appendChild(movieRating)
           movieRating.innerHTML = `<span>${element.vote_average}</span> / 10`
 
-          // Sets up the movie's rating count:
+          // RATINGS AMOUNT:
           const movieRatingCount = document.createElement("h4")
           movieRatingCount.classList.add("movie-rating-count")
           movieRatingBox.appendChild(movieRatingCount)
           movieRatingCount.innerHTML = `${element.vote_count}`
 
-          // Sets up the movie's certification:
+          // CERTIFICATION:
           const releaseDateUrl = `https://api.themoviedb.org/3/movie/${element.id}/release_dates?api_key=${apiKey}`
           const movieCert = document.createElement("h3")
           movieCert.classList.add("movie-cert")
@@ -157,14 +146,9 @@ const showMovies = async (apiUrl, genreUrl) => {
               data.results.forEach(item => {
                 if (item.iso_3166_1 === "US") {
                   item.release_dates.forEach(date => {
-                    // console.log(date)
-
                     if (date.certification) {
                       movieCert.innerHTML = `${date.certification}`
-                    } else if (
-                      !date.certification ||
-                      date.certification == ""
-                    ) {
+                    } else {
                       movieCert.innerHTML = `NR`
                     }
                   })
@@ -174,12 +158,17 @@ const showMovies = async (apiUrl, genreUrl) => {
 
           movieDesc.appendChild(movieCert)
 
-          // Sets up the genre collection:
+          // GENRE COLLECTION:
           const movieGenreBox = document.createElement("div")
           movieGenreBox.classList.add("movie-genre-box")
           movieDetails.appendChild(movieGenreBox)
 
-          // Sets up the genre tags, then plugs them in:
+          // Info for genre tags:
+          const genres = genresList
+            .filter(object => element.genre_ids.includes(object.id))
+            .map(item => item.name)
+
+          // GENRE TAGS:
           genres.map(item => {
             const genreTag = document.createElement("p")
             genreTag.classList.add("movie-genre-tag")
@@ -202,24 +191,33 @@ const showMovies = async (apiUrl, genreUrl) => {
 
 // Function to get a single movie:
 const fetchMovie = elementId => {
+
+  // Sets up some movie search URLs:
   const movieSearchUrl = `https://api.themoviedb.org/3/movie/${elementId}?api_key=${apiKey}&language=en-US`
 
   const releaseDateUrl = `https://api.themoviedb.org/3/movie/${elementId}/release_dates?api_key=${apiKey}`
 
-  // For movie ratings
-  let movieRating = ""
+  // For movie rating:
+  let movieRating;
 
   // Gets the movie's release date and certification:
   fetch(releaseDateUrl)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      data.results.forEach(item => {
+        if (item.iso_3166_1 === "US") {
+          item.release_dates.forEach(date => {
+            if (date.certification) {
+              movieRating = `${date.certification}`
+            } else {
+              movieRating = `NR`
+            }
+          })
+        }
+      })
     })
 
-  // For testing purposes.
-  // console.log(`You clicked me! My ID is: ${elementId}. You can search for me at ${movieSearchUrl}`);
-
-  // Fetches the data for a single movie:
+  // Fetches the data:
   fetch(movieSearchUrl)
     .then(response => response.json())
     .then(data => {
@@ -229,23 +227,58 @@ const fetchMovie = elementId => {
       // Unhides the panel:
       movieCheckbox.checked = true
 
+      // Function for converting runtime to hours and minutes:
+      const timeConvert = (num) => {
+        const number = num
+        const hours = number / 60
+        const rHours = Math.floor(hours)
+        const minutes = (hours - rHours) * 60
+        const rMinutes = Math.round(minutes)
+        return `${rHours}h ${rMinutes}m`
+      }
+
+      // Getting the info for the genres panel:
+      const genres = data.genres.map(genre => `<p class="panel-genre-tag">${genre.name}</p>`)
+
+      console.log(genres);
+
       // Creates a panel to plug information into:
       const panelDetails = document.createElement("section")
-      panelDetails.classList.add("movie-panel-details")
+      panelDetails.classList.add("panel-details")
       panelDetails.innerHTML = `
-        <img class="panel-poster" src="${imgPath}${data.poster_path}">
         <div class="panel-title-block">
           <h3>${data.title}</h3>
-          <h4>${dateFns.format(data.release_date, "YYYY")}</h4>
+          <h4>${dateFns.format(data.release_date, "YYYY")} <span>•</span> ${movieRating} <span>•</span> ${timeConvert(data.runtime)}</h4>
         </div>
+        <div class="panel-summary">
+          <div class="panel-genres-block">
+            ${genres.join(" ")}
+          </div>
+          <p class="summary">${data.overview}</p>
+        </div>
+        <div class="panel-rating-block">
+          <h3><span>${data.vote_average}</span> / 10</h3>
+          <h4>${data.vote_count}</h4>
+        </div>
+        <img class="panel-poster" src="${imgPath}${data.poster_path}">
       `
+
+      // Attaches the details panel to the movie panel:
       moviePanel.appendChild(panelDetails)
 
-      // Closes the panel down:
+      // Adds a blurred BG poster to the movie panel:
+      const panelBgPoster = document.createElement("img")
+      panelBgPoster.classList.add("panel-bg-poster")
+      moviePanel.appendChild(panelBgPoster)
+      panelBgPoster.src = `${imgPath}${data.poster_path}`
+
+
+      // The close button for the pop-up modal:
       closeButton.addEventListener("click", () => {
         movieCheckbox.checked = false
 
         setTimeout(() => {
+          panelBgPoster.remove()
           panelDetails.remove()
         }, 500)
       })
@@ -255,9 +288,6 @@ const fetchMovie = elementId => {
 // +++++++++++++++++++++++++++++++++++++++++++
 // FUNCTION CALLERS
 // +++++++++++++++++++++++++++++++++++++++++++
-
-// Calls the getGenres function:
-// getGenres(genreUrl)
 
 // Calls the showMovies function:
 showMovies(apiUrl, genreUrl)
